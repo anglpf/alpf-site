358838def3fc79567f5aa3d10d03df2dc2a39fcd
Angus Fudge <angf@pm.me>
2022-01-11 12:35:23 -0800

    Removing links to pages under construction

---
+++ on-my-mind/sidebar.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+   document.getElementById("interests-sidebar").style.width = "250px";
+   document.getElementById("interests-main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+   document.getElementById("interests-sidebar").style.width = "0";
+   document.getElementById("interests-main").style.marginLeft= "0";
+}
